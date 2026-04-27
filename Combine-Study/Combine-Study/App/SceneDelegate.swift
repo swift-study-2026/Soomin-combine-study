59e32091a3be06b0a414b1b7dbc53e2024beb69a
sumchive <haon0319@naver.com>
2026-04-27 14:47:29 +0900

    [refactor] 과제별로 폴더링

--- Combine-Study/Combine-Study/App/SceneDelegate.swift
+++ Combine-Study/Combine-Study/App/SceneDelegate.swift
@@ -15,8 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
             guard let windowScene = (scene as? UIWindowScene) else { return }
             
-            let loginViewController = LoginViewController()
-            let navigationController = UINavigationController(rootViewController: loginViewController)
+            let rootViewController = PokeViewController()
+            let navigationController = UINavigationController(rootViewController: rootViewController)
             
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = navigationController
